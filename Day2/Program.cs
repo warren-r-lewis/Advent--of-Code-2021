246b9fcf91da26bf92905b3cd19bb27815b87c4f
Warren Lewis <alphadube@Warrens-MacBook-Pro.local>
2021-12-21 14:05:17 -0800

    added other days

---
+++ Day2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string[] input= RetrieveInput();
+            
+            Console.WriteLine(CaluclateAimAndPosition(input));
+        }
+        public static string[] RetrieveInput(){
+        string[] lines = File.ReadAllLines(@"/Users/alphadube/Documents/C#/Advent of Code/Day2/input.txt");
+        return lines;
+        }
+        public static int PlotPosition(string[] data){
+            int forward_heading = 0;
+             int depth_heading = 0;
+            foreach (string heading in data){
+                if(heading.Contains("forward")){
+                    int forward = int.Parse(heading.Substring(heading.Length -1)); 
+                    forward_heading = forward_heading + forward;
+                }else if(heading.Contains("down")){
+                    int depth= int.Parse(heading.Substring(heading.Length -1)); 
+                    depth_heading = depth_heading + depth;
+                }else if(heading.Contains("up")){
+                    int depth= int.Parse(heading.Substring(heading.Length -1)); 
+                    depth_heading = depth_heading - depth;
+                }
+            }
+            return forward_heading * depth_heading;
+        }
+        public static int CaluclateAimAndPosition(string[] data){
+            int forward_heading = 0;
+            int depth_heading = 0;
+            int aim = 0;
+            foreach (string heading in data){
+                if(heading.Contains("forward")){
+                    int forward = int.Parse(heading.Substring(heading.Length -1)); 
+                    forward_heading = forward_heading + forward;
+                    depth_heading = aim * forward;
+                }else if(heading.Contains("down")){
+                    int depth= int.Parse(heading.Substring(heading.Length -1)); 
+                    depth_heading = depth_heading+depth;
+                    aim = aim + depth;
+                }else if(heading.Contains("up")){
+                    int depth= int.Parse(heading.Substring(heading.Length -1)); 
+                    depth_heading = depth_heading+depth;
+                    aim = aim - depth;
+                }
+            }
+            return forward_heading * depth_heading;
+        }
+    }
+    
+    
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"/Users/alphadube/Documents/C#/Advent of Code/Day3/input.txt");
+            List<int[]>  list = new List<int[]>();
+            foreach (string line in lines)
+            {
+             int[] value = new int[12];
+             int arrayPosition = 0;
+             foreach(Char c in line ){
+                int binaryValue = c - '0';
+                value[arrayPosition]=binaryValue;
+                arrayPosition++;
+                }
+             list.Add(value);
+            }
+            int[] gamma = new int[] {0,0,0,0,0,0,0,0,0,0,0,0};
+            int[] epsilon = new int[] {0,0,0,0,0,0,0,0,0,0,0,0};
+            int[] onesCount = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
+            int[] zeroCount = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
+            foreach(int[] array in list){
+                int arrayIndex = 0;
+                foreach(int i in array){
+                    if(i == 0){
+                        zeroCount[arrayIndex]++;
+                        arrayIndex++;
+                    }
+                    else if(i==1){
+                        onesCount[arrayIndex]++;
+                        arrayIndex++;
+                    }
+                }
+            }
+            for(int i = 0; i < 12; i++){
+                if(onesCount[i] > zeroCount[i]){
+                    gamma[i] = 1;
+                    epsilon[i] =0;
+                }
+                else if(onesCount[i] < zeroCount[i]){
+                    gamma[i] = 0;
+                    epsilon[i] = 1;
+                }
+            }
+            Console.WriteLine();
+            int BinaryConversion(int[] array){
+            int result =0;
+            int binaryPosition = 1;
+            for(int i = array.Length; i-- >0; ){
+              if(array[i] == 1){
+                  result = result + binaryPosition;
+                  binaryPosition = binaryPosition * 2;
+              }
+              else{
+                  binaryPosition = binaryPosition * 2;
+              }
+            }   
+            return result;
+            }
+
+            Console.WriteLine(BinaryConversion(gamma)*BinaryConversion(epsilon));
+        }
+    }
+}
